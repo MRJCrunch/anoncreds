c6b62c8f679aad659d7b50909971056355ad69ac
Evgeniy Razinkov <evgeniy.razinkov@dsr-company.com>
2017-07-10 16:31:30 +0300

    test_works

--- anoncreds/test/test_interoperability_with_libsovrin.py
+++ anoncreds/test/test_interoperability_with_libsovrin.py
@@ -17,6 +17,7 @@ from anoncreds.test.conftest import GVT, XYZCorp, primes1
 from anoncreds.protocol.utils import toDictWithStrValues, \
     fromDictWithStrValues, deserializeFromStr, encodeAttr, crypto_int_to_str, to_crypto_int
 loop = asyncio.get_event_loop()
+init_dict = {}
 
 
 def main():
@@ -30,27 +31,33 @@ def main():
     while True:
         data = json.loads(conn.recv(1024).decode("utf-8"))
         print(data)
+        if ('type' in data) & (data['type'] == 'get_claim_def'):
+            print('get_claim_def -> start')
+            init = asyncio.ensure_future(issuer_init(primes1(), conn))
+            loop.run_until_complete(init)
+            print('get_claim_def -> done')
         if ('type' in data) & (data['type'] == 'issue'):
-            print('in issuer')
+            print('issue_claim -> start')
             # loop = asyncio.get_event_loop()
             # loop.run_until_complete(issuer_init(primes1(), conn, data['data']['blinded_ms']))
             # loop.close()
-            future = asyncio.ensure_future(issuer_init(primes1(), conn, data['data']['blinded_ms']))
+            future = asyncio.ensure_future(issue_claim(conn, data['data']['blinded_ms']))
             loop.run_until_complete(future)
             print('!!!!!!!!!after loop!!!!!!!!!!')
             print(data['data'])
             print('sent an answer')
+            print('issue_claim -> done')
         if (('type' in data) & (data['type'] == 'close')) | (not data):
             break
 
     sock.close()
 
-async def issuer_init(primes, conn, claim_request):
+async def issuer_init(primes, conn):
     # 1. Init entities
     publicRepo = PublicRepoInMemory()
     attrRepo = AttributeRepoInMemory()
     issuer = Issuer(IssuerWalletInMemory('issuer1', publicRepo), attrRepo)
-    #import pudb;pudb.set_trace()
+
     # 2. Create a Schema
     schema = await issuer.genSchema('GVT', '1.0', GVT.attribNames())
     schemaId = ID(schema.getKey())
@@ -68,19 +75,23 @@ async def issuer_init(primes, conn, claim_request):
     attrRepo.addAttributes(schema.getKey(), userId, attrs)
 
     public_key = await issuer.wallet.getPublicKey(schemaId)
-    claim_request = ClaimRequest.from_str_dict(claim_request, public_key.N)
+    global init_dict
+    init_dict = {
+        'schemaId': schemaId,
+        'public_key': public_key,
+        'issuer': issuer
+    }
+    conn.send(json.dumps({
+        'primary': public_key.to_str_dict(),
+        'revocation': None
+    }).encode())
+
+
+async def issue_claim(conn, claim_request):
+    claim_request = ClaimRequest.from_str_dict(claim_request, init_dict['public_key'].N)
     print(claim_request)
     print('before issueClaim')
-    (signature, claims) = await issuer.issueClaim(schemaId, claim_request)
-    # claims = {
-    #        'primaryClaim': {
-    #             'm2': str(crypto_int_to_str(claims.primaryClaim.m2)),
-    #             'a': str(crypto_int_to_str(claims.primaryClaim.A)),
-    #             'e': str(claims.primaryClaim.e),
-    #             'v': str(claims.primaryClaim.v)
-    #         },
-    #        'nonRevocClaim': None
-    #     }
+    (signature, claims) = await init_dict['issuer'].issueClaim(init_dict['schemaId'], claim_request)
 
     msg = {
         'signature': signature.to_str_dict(),
